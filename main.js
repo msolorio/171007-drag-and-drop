3386cd31c617c9448c764f4fd3ea468f3f5813de
msolorio <msolorio789@gmail.com>
2017-10-08 09:40:16 -0700

    add functionality to increment items in cart

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ window.onload = function() {
   const items = document.querySelectorAll('.item');
   const cart = document.querySelector('.cart');
 
+  const itemsInCart = {};
+
   function handleDragStart(event) {
     event.dataTransfer.setData('text', event.target.id);
   }
@@ -12,13 +14,29 @@ window.onload = function() {
     event.dataTransfer.dropEffect = 'copy';
   }
 
+  function buildItemsInCart() {
+    cart.innerHTML = '';
+
+    for (itemId in itemsInCart) {
+      const item = document.getElementById(itemId);
+      const itemClone = item.cloneNode(true);
+      const span = document.createElement('span');
+      span.classList.add('itemAmount');
+      span.innerHTML = `${itemsInCart[itemId]}x`;
+      itemClone.appendChild(span);
+      cart.appendChild(itemClone);
+    }
+  }
+
   function handleDrop(event) {
     event.preventDefault();
 
     const itemId = event.dataTransfer.getData('text');
-    const item = document.getElementById(itemId);
-    const itemClone = item.cloneNode(true);
-    cart.appendChild(itemClone);
+
+    if (!itemsInCart[itemId]) itemsInCart[itemId] = 1;
+    else itemsInCart[itemId]++;
+
+    buildItemsInCart();
   }
 
   items.forEach(item => {
